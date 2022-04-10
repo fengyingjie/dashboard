adb6cbf2a708e19f18eac3f0fb8f28ee3a79dea5
fengyingjie <feng_yingjie1982@163.com>
2022-04-10 23:51:03 +0800

    初始化提交

---
+++ src/components/FileIdList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="example1">
+    <HotTable :data= "data" :columHaeder=true :licenseKey="licenseKey">
+      <HotColumn title="fileName"></HotColumn>
+      <HotColumn title="project"></HotColumn>
+      <HotColumn title="sub"></HotColumn>
+      <HotColumn title="id"></HotColumn>
+    </HotTable>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { HotTable,HotColumn } from '@handsontable/vue3';
+//import Handsontable from 'handsontable/base';
+import { registerAllModules } from 'handsontable/registry';
+
+
+// register Handsontable's modules
+registerAllModules();
+
+const data = [
+  ['c:\\***\\aaa\\aaa-web\\src\\main\\com\\a\\aaa\\H001\\contrllor\\H001C01.java','','',''],
+  ['c:\\***\\aaa\\aaa-web\\src\\main\\com\\a\\aaa\\H001\\contrllor\\H001C02.java','','',''],
+  ['c:\\***\\aaa\\aaa-web\\src\\main\\com\\a\\aaa\\H001\\contrllor\\H001C03.java','','','']
+];
+const licenseKey = 'non-commercial-and-evaluation';
+</script>
+
+<style src="handsontable/dist/handsontable.full.css"></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import FileIdList from "./FileIdList.vue";
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+</script>
+
+<template>
+{{msg}}
+<form class="fileInput">
+  <label>file:</label><input type="text" name="fileName" value="" />
+</form>
+<FileIdList></FileIdList>
+</template>
+
+<style scoped>
+.fileInput{
+  text-align:left;
+}
+</style>
